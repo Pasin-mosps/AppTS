9541b4a827048d0da2ee5636ca62d7bb5f76fcf3
Pasin <mossps@Pasins-MacBook-Pro.local>
2024-04-20 21:24:51 +0700

    first commit

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import Container from "../Container"
+
+const Navbar = () => {
+    return (
+        <div className="fixed w-full bg-white z-10 shadow-sm">
+            <div className=" py-4 bopprder-b-[1px]">
+                <Container>
+                    Yoy
+                </Container>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <main>
+    <div className = "text-rose-500 test-2xl">home</div>
+    <div>home</div>
+    <div>home</div>
+    <div>home</div>
+   </main>
+  );
+}
